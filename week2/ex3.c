6c8fe96765f933dc0746de1a3750bfeb3f8c6b31
user <spirys856@mail.ru>
2018-08-29 23:44:10 +0300

    minor changes

--- week2/ex3.c
+++ week2/ex3.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
-	int n; 
-	sscanf(argv[1], "%i", &n);
+    int n;
+    sscanf(argv[1], "%i", &n);
 
-	int size = n * 2 - 1;
+    int size = n * 2 - 1;
 
-	for (int i = 0; i < n; ++i)
-	{
-		char* str;
-		str = (char*)malloc(size);
+    for (int i = 0; i < n; ++i)
+    {
+        char *str;
+        str = (char *)malloc(size);
 
-		int j;
-		for (j = 0; j < (size-1)/2-i; ++j)
-		{
-			str[j] = ' ';
-		}
+        int j;
+        for (j = 0; j < (size - 1) / 2 - i; ++j)
+        {
+            str[j] = ' ';
+        }
 
-		for (; j < (size-1)/2+i+1; ++j)
-		{
-			str[j] = '*';
-		}
+        for (; j < (size - 1) / 2 + i + 1; ++j)
+        {
+            str[j] = '*';
+        }
 
-		for (; j < size; ++j)
-		{
-			str[j] = ' ';
-		}
+        for (; j < size; ++j)
+        {
+            str[j] = ' ';
+        }
 
-		printf("%s\n", str);
-	}
+        printf("%s\n", str);
+    }
 }
